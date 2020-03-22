87b1aaa9689a11105faf1386f2e97e3b6e6fe7aa
Rishikesh Maurya <rishikesh.maurya@myntra.com>
2020-03-22 10:47:59 +0530

    added webview

--- app/src/main/java/com/technozoneitcare/MainActivity.java
+++ app/src/main/java/com/technozoneitcare/MainActivity.java
@@ -2,13 +2,35 @@ package com.technozoneitcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView myWebView;
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //fetch website 
+        myWebView = findViewById(R.id.webview);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("https://technozoneitcare.netlify.com/");
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (myWebView.canGoBack()) {
+            myWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
